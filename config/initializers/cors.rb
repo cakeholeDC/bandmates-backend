14a7a700ec358710978c6b174360b8cc831986c2
Kyle <kylepcole@gmail.com>
2020-03-04 15:08:02 -0500

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bandmates-app.herokuapp.com/'
+    origins 'http://bandmates-app.herokuapp.com/'
 
     resource '*',
       headers: :any,
